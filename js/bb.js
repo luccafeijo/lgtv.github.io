82bbf94b071004da3818755ca3edad6ea462b7a9
luccafeijo <luccafeijo23@gmail.com>
2023-05-29 00:37:03 -0300

    exibicao ok
    exibicao adequada de meses

--- js/bb.js
+++ js/bb.js
@@ -1,5 +1,4 @@
-// const inicio = new Date('2023-04-12 00:00:00');
-const inicio = new Date('2023-04-30 00:00:00');
+const inicio = new Date('2023-04-12 00:00:00');
 
 const hoje = new Date();
 
@@ -7,18 +6,25 @@ $('#box-mes-dia').html(MostraMesDias(inicio, hoje));
 $('#box-semanas').html(MostraSemanasDias(inicio, hoje));
 
 function MostraMesDias(inicio, hoje) {
-    const totaldias = Math.ceil(Math.abs(hoje - inicio) / (1000 * 60 * 60 * 24));
-    var mesinicio = BuscaDiaUm(inicio);
-    var meshoje = BuscaDiaUm(hoje);
     var exibir = "";
+    // const totaldias = Math.ceil(Math.abs(hoje - inicio) / (1000 * 60 * 60 * 24));
 
-    console.log(mesinicio);
-    console.log(meshoje);
+    //diferença de dias entre o dia 1 e o dia atual
+    var diff = inicio.getDate() -1;
 
+    //removendo a diferença, a data vai ficar como dia 1
+    inicio.setDate(inicio.getDate() - diff);
+    var mesinicio = Math.abs(inicio.getMonth()+1); //+1 apenas para deixar a numeracao correta, ja que "janeiro" começa com o valor "0"
+    // logData(inicio);
 
+    //removendo a diferença que possuia do inicio, hoje pode acabar mudando de mês, e com isso, verificar se é diferente do mês de inicio
+    hoje.setDate(hoje.getDate() - diff);
+    var meshoje = Math.abs(hoje.getMonth()+1); //+1 apenas para deixar a numeracao correta, ja que "janeiro" começa com o valor "0"
+    // logData(hoje);
+
+    exibir = meshoje - mesinicio;
      
-    return "<p>"+totaldias+"</p>";
-    return "<p>x mês e x dias</p>";
+    return "<p>"+exibir+" meses</p>";
 }
 
 function MostraSemanasDias(inicio, hoje) {
@@ -27,10 +33,12 @@ function MostraSemanasDias(inicio, hoje) {
     var dias = 0;
     var exibir = "";
 
+    //contagem de semanas (sem valor quebrado igual em totaldias/7)
     for (var i = 0; i <= totaldias/7; i++) {
         semanas = i;
     }
 
+    //verifica os dias que faltaram para completar uma semana
     dias = totaldias - (7*semanas);
      
     if (dias > 0) {
@@ -57,4 +65,8 @@ function BuscaDiaUm(data) {
     // for (var i = 0; i < mesData; i++) {
     //     dataCalc.setDate(dataCalc.getDate() - 1);
     // }
+}
+
+function logData(data) {
+    console.log(data.getDate() + "/" + Math.abs(data.getMonth()+1) + "/" + data.getFullYear());
 }
