02708732236764548cbb3e17e22f00da1ecd75e4
luccafeijo <lucca.feijo@cpninformatica.com.br>
2024-05-07 13:47:19 -0300

    geracao aleatoria de bombas

--- js/campo-minado.js
+++ js/campo-minado.js
@@ -29,12 +29,16 @@ function montaTabela (difficulty) {
     //8
     if (difficulty == 'easy') {
         size = 5;
+        bombs = espalhaBombas(4, 25);
     } else if (difficulty == 'medium') {
         size =10;
+        bombs = espalhaBombas(25, 100);
     } else if (difficulty == 'hard') {
         size = 15;
+        bombs = espalhaBombas(80, 225);
     } else if (difficulty == 'rufino') {
         size = 40;
+        bombs = espalhaBombas(700, 1600);
     }
 
     var finalboard = '<div class="game-case">' +
@@ -51,11 +55,18 @@ function montaTabela (difficulty) {
     '</div>' +
     '<div class="gameplay">';
 
+    var contagem = 0;
+
     for (var i = 0; i < size; i++) {
         finalboard += "<div class='row'>";
 
         for (var y = 0; y < size; y++) {
-            finalboard += "<div class='square'><p></p></div>";
+            contagem++;
+            if(bombs.includes(contagem)) {
+                finalboard += "<div class='square'><p>?</p></div>";
+            } else {
+                finalboard += "<div class='square'><p></p></div>";
+            }
             // console.log(finalboard.length);
         }
 
@@ -73,19 +84,33 @@ function animacaoTitulo(){
     
     let h1 = document.getElementById("wave_animated");
 
-        // console.log(h1.innerHTML.split(""));
-        text = h1.innerHTML;
-        h1.innerHTML = text
-        .split("")
-        .map(letter => {
-            return `<span>` + letter + `</span>`;
-        })
-        .join("");
+    // console.log(h1.innerHTML.split(""));
+    text = h1.innerHTML;
+    h1.innerHTML = text
+    .split("")
+    .map(letter => {
+        return `<span>` + letter + `</span>`;
+    })
+    .join("");
 
-        Array.from(h1.children).forEach((span, index) => {
+    Array.from(h1.children).forEach((span, index) => {
         setTimeout(() => {
             span.classList.add("wavy");
         }, index * 60 + delay);
-        });
-    
+    });
 }
+
+function espalhaBombas(quantidade, totalArea) {
+    var array = [];
+    while (array.length < quantidade) {
+        var randomNumber = getRandomNumber(1, totalArea);
+        if (!array.includes(randomNumber)) {
+          array.push(randomNumber);
+        }
+    }
+    return array;
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
