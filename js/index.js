2b3fb720e82ecff830353f9b3277f704d41bd1ec
luccafeijo <lucca.feijo@cpninformatica.com.br>
2024-06-20 09:12:13 -0300

    incluindo fortnite2 no index

--- js/index.js
+++ js/index.js
@@ -1,27 +1,50 @@
 $( document ).ready(function() {
     randomTextCampoMinado();
+    randomTextFortnite2();
 });
 
 function randomTextCampoMinado() {
     var randomNumber = Math.floor(Math.random() * 6);
-    console.log(randomNumber);
     switch(randomNumber) {
         case 1:
-            $('.ondev').html("V 1.0!");
+            $('.minesweepergame').html("V 1.0!");
           break;
         case 2:
-            $('.ondev').html("Isso não é uma cópia de Minecraft");
+            $('.minesweepergame').html("Isso não é uma cópia de Minecraft");
           break;
         case 3:
-            $('.ondev').html("Bomba bomba, olha a bomba!");
+            $('.minesweepergame').html("Bomba bomba, olha a bomba!");
           break;
         case 4:
-            $('.ondev').html("GOTY");
+            $('.minesweepergame').html("GOTY");
             break;
         case 5:
-            $('.ondev').html("Ouvi dizer que tem cheats...");
+            $('.minesweepergame').html("Ouvi dizer que tem cheats...");
           break;
         default:
           // code block
       }
+}
+
+function randomTextFortnite2() {
+  var randomNumber = Math.floor(Math.random() * 6);
+  switch(randomNumber) {
+      case 1:
+          $('.fortnite2game').html("We got a number one Victory Royale");
+        break;
+      case 2:
+          $('.fortnite2game').html("Hurr Hurr-Hur-Hurr Hurr Hurr-Hur-Hurr Hur-Hurr");
+        break;
+      case 3:
+          $('.fortnite2game').html("SAAAANSSSSS!");
+        break;
+      case 4:
+          $('.fortnite2game').html("Best Game Ever Made");
+          break;
+      case 5:
+          $('.fortnite2game').html("Submit your speedrun!");
+        break;
+      default:
+        // code block
+    }
 }
