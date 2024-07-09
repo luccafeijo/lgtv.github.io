a466c94758728f482c89e108fcfc071e77552731
luccafeijo <lucca.feijo@cpninformatica.com.br>
2024-07-09 13:36:55 -0300

    pre cache images js

--- js/paulin.js
+++ js/paulin.js
@@ -1,20 +1,38 @@
 $( document ).ready(function() {
-    action = setInterval(function() {
-        $('#character').removeClass("work");
-        $('#character').removeClass("still");
-        $('#character').removeClass("look");
-        
-        var randomNumber = Math.floor(Math.random() * 3);
-        
-        switch(randomNumber) {
-            case 1:
-                $('#character').addClass("work");
-              break;
-            case 2:
-                $('#character').addClass("look");
-              break;
-            default:
-                $('#character').addClass("still");
-          }
-    }, 1000);
+
+  cacheImages();
+  
+  action = setInterval(function() {
+      $('#character').removeClass("work");
+      $('#character').removeClass("still");
+      $('#character').removeClass("look");
+      
+      var randomNumber = Math.floor(Math.random() * 3);
+      
+      switch(randomNumber) {
+          case 1:
+              $('#character').addClass("work");
+            break;
+          case 2:
+              $('#character').addClass("look");
+            break;
+          default:
+              $('#character').addClass("still");
+        }
+  }, 1000);
 });
+
+function cacheImages(){
+  preloads = [
+    'paulin_still.png',
+    'paulin_work.png',
+    'paulin_look.png'
+  ];
+
+  var tempImg = []
+
+  for(var x=0;x<preloads.length;x++) {
+      tempImg[x] = new Image()
+      tempImg[x].src = '../sources/images/paulinpim-game/'.preloads[x]
+  }
+}
