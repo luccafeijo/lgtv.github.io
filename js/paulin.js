676289972d60c8fafeea3111e33b1e988c4c99ca
luccafeijo <luccafeijo23@gmail.com>
2024-07-11 20:24:51 -0300

    funcionando upgrade de spotify porem com bug na soma JS

--- js/paulin.js
+++ js/paulin.js
@@ -3,16 +3,28 @@ var working;
 
 //dinheiro no jogo
 var money = 0;
+var clickValue = 1;
+var passiveMoney = 0;
+
+//upgrades secundarios
+var vscode = 0;
+var vscodeCustoInicial = 20;
+var vscodeCustoAdicional = 20;
+
+var spotify = 0;
+var spotifyCustoAtual = 10;
+var spotifyCustoAdicional = 2;
 
 $( document ).ready(function() {
 
   cacheImages();
+  attPassiveMoney();
 
   $('.main-action').on('click', function(){
     clearInterval(working);
     $('#character').removeClass("still");
     $('#character').addClass("work");
-    addMoney(1);
+    addMoney(clickValue);
 
     working = setTimeout(function() {
       $('#character').removeClass("work");
@@ -21,13 +33,47 @@ $( document ).ready(function() {
   });
 
   $('.secondary-action').on('click', function(){
+    switch(this.id) {
+      case 'spotify':
+        // console.log(money);
+        // console.log(spotifyCustoAtual);
+        if(money >= spotifyCustoAtual && spotify < 10) {
+          money -= spotifyCustoAtual;
+          exibeMoney();
+          spotifyCustoAtual += spotifyCustoAdicional;
+          spotify++;
+          $('.'+this.id+'-progress').html(spotify+'/10');
+          $('.'+this.id+'-value').html(spotifyCustoAtual + ' Paulocoins');
+          passiveMoney += 0.1;
+
+          //TODO
+          // ta bugado, o passive money, ao acressentar 0.1 de valor, as vezes fica com o número quebrado, tipo "0.30000000000000004" (exemplo real)
+          // console.log(passiveMoney);
+        }
+        break;
+      case 'vs_code':
+        clickValue = clickValue * 2;
+        break;
+      default:
+        // code block
+    }
     console.log(this.id);
   });
 });
 
 function addMoney(value){
   money += value;
-  $('#money').html(money);
+  exibeMoney();
+}
+
+function exibeMoney() {
+  $('#money').html(money.toFixed(2).replace(".", ","));
+}
+
+function attPassiveMoney(){
+  setInterval(function() {
+    addMoney(passiveMoney);
+  }, 1000);
 }
 
 function cacheImages(){
